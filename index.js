a65751f1ffaa0db91928d84f2eec3fb387f59165
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-30 17:36:28 +0100

    Create array obj with title and snippet and username with string and render them

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const express = require('express')
 
 const app = express()
+
+
+app.get('/', (req, res) => {
+    const day = [
+        { title: 'Good day', snippet: 'I had a good gym workout' },
+        { title: 'Okay day', snippet: 'I did not do much today' },
+        { title: 'Bad day', snippet: 'I fell on floor and got a big bruise' }
+    ]
+    res.render({ username: 'mouazjamal', blogs: day })
+})
