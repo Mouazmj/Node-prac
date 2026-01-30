1ef583a4a20b2b8d03900f7bd8525ae1fa70baf1
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-30 16:12:49 +0100

    Now did the same but using the framework express

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile('./views/index.html', { root: __dirname })
+})
+
+app.get('/about', (req, res) => {
+    res.status(200).sendFile('./views/about.html', { root: __dirname })
+})
+
+app.get('about-me', (req, res) => {
+    res.status(301).redirect('/about')
+})
+
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', { root: __dirname })
+})
+
+
+
+app.listen(3000)
