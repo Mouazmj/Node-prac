099ce07304a4dda54b3d393662ae6316af682667
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-30 18:01:14 +0100

    Now done!

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.listen(3000)
+
+app.set('view engine', 'ejs')
+
 
 app.get('/', (req, res) => {
     const day = [
@@ -9,5 +13,6 @@ app.get('/', (req, res) => {
         { title: 'Okay day', snippet: 'I did not do much today' },
         { title: 'Bad day', snippet: 'I fell on floor and got a big bruise' }
     ]
-    res.render({ username: 'mouazjamal', blogs: day })
+    res.render('partials/head', { username: 'mouazjamal', blogs: day, title: 'head' })
 })
+
