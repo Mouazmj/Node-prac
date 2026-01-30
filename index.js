d11c0cfeeef0a32bc21dfb582a5908f2e66b02bc
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-30 15:18:16 +0100

    Done no commits just small prac to rember stuff

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const http = require('http')
 const fs = require('fs')
-const path = './views'
+
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
+    let path = './views'
 
+    if (req.url === '/') {
+        res.statusCode = 200
+        path += '/index.html'
+    } else if (req.url === '/about') {
+        res.statusCode = 200
+        path += '/about.html'
+    } else if (req.url === '/om-mig') {
+        res.statusCode = 301
+        res.redri('Location', '/about')
+        res.end()
+        return;
+    } else {
+        res.statusCode = 404
+        path += '/404.html'
     }
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err)
+            res.statusCode = 500
+            res.end('Error in server')
+        } else {
+            res.end(data)
+        }
+    })
+})
+
+server.listen(3000, 'localhost', () => {
+    console.log('Server running on port 3000')
 })
